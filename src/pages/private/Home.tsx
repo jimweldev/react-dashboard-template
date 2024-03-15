a9f37cb4c3c9b2e87f962629c6d346f21f3d89ef
Jimwel Dizon <jimweldizon.dev@gmail.com>
2024-03-15 16:02:58 +0800

    Added react hook form and zod

--- src/pages/private/Home.tsx
+++ src/pages/private/Home.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { toast } from "sonner";
 
 const Home = () => {
@@ -21,9 +27,37 @@ const Home = () => {
             error: "Error",
           });
         }}
+        size="sm"
       >
         Toast
       </Button>
+
+      <div className="grid grid-cols-12 gap-3">
+        <Card className="col-span-12 md:col-span-4 lg:col-span-3">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="col-span-12 md:col-span-4 lg:col-span-3">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="col-span-12 md:col-span-4 lg:col-span-3">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="col-span-12 md:col-span-4 lg:col-span-3">
+          <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+          </CardHeader>
+        </Card>
+      </div>
     </>
   );
 };
--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -8,45 +8,94 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import useAuthUserStore from "@/store/authUserStore";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const { setAuthUser } = useAuthUserStore((state: any) => ({
     setAuthUser: state.setAuthUser,
   }));
 
+  const formSchema = z.object({
+    email: z.string().email({
+      message: "Invalid email address.",
+    }),
+    password: z.string().min(1, {
+      message: "Password is required.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setAuthUser({ firstName: "Jimwel Dizon", ...values });
+  }
+
   return (
     <>
-      <Card className="w-[380px] mx-auto">
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Welcome back!</CardTitle>
-          <CardDescription>Login to your account to continue</CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button
-            className="w-full"
-            onClick={() => {
-              setAuthUser({
-                firstName: "Jimwel Dizon",
-                email: "jimwel@gmail.com",
-              });
-            }}
-          >
-            Login
-          </Button>
-        </CardFooter>
-      </Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <Card className="w-[380px] mx-auto max-w-full">
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-2xl">Welcome back!</CardTitle>
+              <CardDescription>
+                Login to your account to continue
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="m@example.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
     </>
   );
 };
